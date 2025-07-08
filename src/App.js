1d8ffe388383e7631d2a6160600b8ecda4532bfb
Rupak Lama <rupaklamadev@gmail.com>
2025-07-08 18:54:14 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className='App'>
+      <header className='App-header'>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <code>YouDrink.com</code> is on sale!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+        Contact here
+        <a className='App-link' href='mailto:lamarupu@gmail.com' target='_blank' rel='noopener noreferrer'>
+          youdrinkdotcom@gmail.com
         </a>
       </header>
     </div>
-  );
+  )
 }
 
 export default App;
